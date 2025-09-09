02063564c00741cf469bb4fcc047e5a38de22781
smiRaphi <neogt404@gmail.com>
2025-09-09 22:07:02 +0200

    auto download keys

--- README.md
+++ README.md
@@ -3,11 +3,6 @@
 ---
 Usage: `python unipyx.py <input> [<output dir>]`
 
-### ps3key.py
-You need to provide the disc keys to extract PS3 ISOs, you can do so by making a folder called `ps3keys` in `bin/` containing `<game name>.dkey` files and afterwards running `ps3key.py` from the `bin/` folder.
-### wiiudk.py
-You need to provide disc keys to extract Wii U WUD/WUX files, you can do so by making a `keys.txt` file in `bin/` with one key per line formatted like this: `<key> # <game name>` and afterwards running `wiiudk.py` from the `bin/` folder.
-
 ## Included tools
 * xbp.bms by [h3x3r](https://reshax.com/profile/183-h3x3r/) taken from https://reshax.com/topic/18058-ripping-sounds-from-fuzion-frenzy-xbox/
 * self compiled [hac2l](https://github.com/Atmosphere-NX/hac2l)
--- bin/ps3key.py
+++ bin/ps3key.py
@@ -6,43 +6,42 @@ class PS3Keys:
     def __init__(self):
         self.load()
     def load(self):
+        if not os.path.exists(__db__): makedb()
+
         i = open(__db__,'rb')
         self._db = {}
         while True:
-            sln = i.read(1)
-            if not sln: break
-            nm = i.read(sln[0]).decode()
+            id1 = i.read(4)
+            if not id1: break
+            zs = i.read(1)[0]
+            nm = id1.decode() + '0'*zs + str(int.from_bytes(i.read(3),'little'))
             self._db[nm] = i.read(16).hex().upper()
+        i.close()
 
     def get(self,key:str) -> str|None:
-        if os.path.exists(key) or '\\' in key or '/' in key:
-            if not os.path.isdir(key) or key.endswith('.iso'): key = os.path.splitext(os.path.basename(key))[0]
-            else: key = os.path.basename(key)
-        key = fmt(key)
-        return self._db.get(key)
+        assert os.path.exists(key) and os.path.isfile(key)
+        f = open(key,'rb')
+        f.seek(0x800)
+        assert f.read(12) == b'PlayStation3'
+        f.seek(4,1)
+        key = f.read(0x20).strip().decode().replace('-','')
+
+        return self._db.get(key) or self.__db['TEST01814']
     def __getitem__(self,key): return self.get(key)
 
-def fmt(i:str): return ''.join(x for x in re.sub(r'\(\w\w,[\w,]+\)','',i.lower()) if x.isalnum())
-def makedb(dr:str,out:str):
-    o = open(out,'wb')
-    p = nm = ''
-    for x in os.listdir(dr):
-        if not x.endswith('.dkey'): continue
-        print(x)
-        p = os.path.join(dr,x)
-        nm = fmt(x[:-5]).encode()
-        assert len(nm) <= 0xff,f"Name too long {nm}"
-        o.write(len(nm).to_bytes(1,'little'))
-        o.write(nm)
-        o.write(bytes.fromhex(open(p).read().strip()))
+def makedb():
+    import httpx
+
+    ks = re.findall(r'">(\w*)</a><TD>.+<TD>([A-Fa-f\d]*)</TR>',httpx.get('https://ps3.aldostools.org/dkey.html',verify=False).text)
+
+    o = open(__db__,'wb')
+    for i,k in ks:
+        if not i: continue
+        o.write(i[:4].encode())
+        o.write((len(i[4:])-len(i[4:].lstrip('0'))).to_bytes(1,'little'))
+        o.write(int(i[4:]).to_bytes(3,'little'))
+        o.write(bytes.fromhex(k) if k else b'\0'*16)
     o.close()
 
 if __name__ == '__main__':
-    from sys import argv
-
-    if len(argv) > 1: dr = argv[1]
-    else: dr = 'ps3keys'
-    if len(argv) > 2: out = argv[2]
-    else: out = __db__
-    try: makedb(dr,out)
-    except Exception as e: input(e)
+    makedb()
